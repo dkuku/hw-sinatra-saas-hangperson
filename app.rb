cd577517aa8fd286643554614cfece17caf27b93
dkuku <daniel.kuku@gmail.com>
2018-09-28 01:31:32 +0100

    Repeated guess scenario passing

--- app.rb
+++ app.rb
@@ -39,7 +39,23 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    begin
+      returned_value = @game.guess letter
+      if letter == returned_value
+        redirect '/show'
+      end
+
+      if  !returned_value
+        if @game.wrong_guesses == letter || @game.guesses == letter
+          flash[:message] = "You have already used that letter"
+        else
+          flash[:message] = "Invalid Guess"
+        end
+      end
+    rescue ArgumentError
+      flash[:message] = "Invalid argument given"
+    end
+
     redirect '/show'
   end
   
@@ -49,8 +65,15 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    state =  @game.check_win_or_lose
+    case state
+    when state == :win
+      redirect '/win'
+    when state == :lose
+      redirect '/lose'
+    else
+      erb :show
+    end
   end
   
   get '/win' do
